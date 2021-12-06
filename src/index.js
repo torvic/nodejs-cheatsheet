8628f82fddc2dde69eb64db8fc812f5078d2b8b2
torvic <130111@unsaac.edu.pe>
2021-12-06 16:50:45 -0500

    Feat: nodejs session with connect-flash

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 const express = require('express')
-
 const app = express()
 const path = require('path')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // settings
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 // middlewares
-
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(
+  session({
+    secret: 'mysecretkey',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+app.use(flash())
+//global variables
+app.use((req, res, next) => {
+	app.locals.message = req.flash('success')
+	next()
+})
 // Routes
 app.use(require('./routes/index'))
 
 app.listen(4000)
-console.log('Server on port', 4000);
+console.log('Server on port', 4000)
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,21 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-	res.render('index')
+  res.render('index')
 })
 
 router.post('/register', (req, res) => {
+  console.log(req.body)
+	// req.flash('user', req.body)
 
+	req.flash('success', 'Now you are registered')
+  res.redirect('/profile')
 })
 
 router.get('/profile', (req, res) => {
-
+  res.render('profile')
+})
+router.get('/products', (req, res) => {
+	res.render('products')
 })
 module.exports = router
