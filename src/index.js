857082c08bb4c91c542e6530216bd12a4c31ca07
torvic <130111@unsaac.edu.pe>
2021-12-10 08:24:22 -0500

    Express basic

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 const express = require('express')
-const path = require('path')
 
 // initialize express app
 const app = express()
 
 // settings
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 
 // middlewares
+app.use(express.urlencoded({ extended: true }))
 
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  )
+  // next()
+})
+app.use('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>')
+})
 
 // Routes
 
 // server listen
 app.listen(4000)
-console.log('Server on port', 4000);
+console.log('Server on port', 4000)
