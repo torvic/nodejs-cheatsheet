61ef4f00c070f7f96ea6cda383cf45ff2575d3a7
torvic <130111@unsaac.edu.pe>
2021-12-14 18:17:33 -0500

    I add the product details functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const path = require('path')
 
+// import routes
+const adminRoutes = require('./routes/admin.routes')
+const shopRoutes = require('./routes/shop.routes')
+
+// error controller
+const { get404 } = require('./controllers/error.controller')
+
 // initialize express app
 const app = express()
 
@@ -9,9 +16,14 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 // middlewares
+app.use(express.urlencoded({ extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 // Routes
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+app.use(get404)
 
 // server listen
 app.listen(4000)
---
+++ src/models/products.model.js
@@ -0,0 +1,37 @@
+// importo fs -> handle of files
+const { readFile, writeFile } = require('fs/promises')
+const path = require('path')
+
+// Get the product from file
+const productsPath = path.join(__dirname, '../data', 'products.json')
+console.log('yep products path', productsPath)
+const getProductsFromFile = async () => {
+  try {
+    const products = await readFile(productsPath);
+    return JSON.parse(products.toString());
+  } catch (error) {
+    console.log('File not found', error);
+    return [];
+  }
+};
+class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  async save() {
+    this.id = Math.random().toString();
+    const products = await getProductsFromFile();
+    products.push(this);
+    await writeFile(productsPath, JSON.stringify(products));
+  }
+
+  static async fetchAll() {
+    return await getProductsFromFile();
+  }
+}
+
+module.exports = Product
